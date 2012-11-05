f27fd79cc8a317e06ca1b3f1c6e4504ee8f6d220
Sam Rosenblum <rosenblum.sam@gmail.com>
2012-11-05 01:08:12 -0500

    Fixed to pass import test

--- launchers/demo_robot.py
+++ launchers/demo_robot.py
@@ -2,7 +2,7 @@
 # directory that robot.py is located, relative to this file
 robot_path = '../demorobot'
 
-import snake
-
+import run_test
+snake = run_test.wpilib.load_module('snake', '/snake/__init__.py')
 def run_tests(robot_module, myrobot):
     snake.launch_robot(robot_module, myrobot)
