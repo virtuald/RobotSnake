9b94e3331f204ef6ad0e1870b6a72c8295a69015
Dustin Spicuzza <dustin@virtualroadside.com>
2012-11-09 01:30:02 -0500

    Added working watchdog implementation for RobotSnake

--- snake/snake_wpilib.py
+++ snake/snake_wpilib.py
@@ -5,6 +5,7 @@
     In particular, time needs to be different
 '''
 
+import sys
 import time
 import threading
 
@@ -12,7 +13,7 @@ import fake_wpilib as wpilib
 import fake_wpilib.fake_time
 
 
-class FakeTime(object):
+class SnakeTime(object):
     
     def __init__(self):
         self.time = 0
@@ -39,8 +40,129 @@ class FakeTime(object):
         if notifier in self.notifiers:
             self.notifiers.remove( notifier )
 
-fake_wpilib.fake_time.FAKETIME = FakeTime()
+fake_wpilib.fake_time.FAKETIME = SnakeTime()
 
+class SnakeWatchdog(object):
+    '''
+        This is a watchdog implementation for the Snake game. If you
+        spend too much time not executing Robot code, then this will
+        kill your robot. 
+        
+        When the watchdog detects a problem, it will call the function
+        stored in SnakeWatchdog.watchdog_handler. The default handler
+        prints out an error message. 
+        
+        This class should be fully threadsafe.
+    '''
+    
+    kDefaultWatchdogExpiration = fake_wpilib.Watchdog.kDefaultWatchdogExpiration
+    
+    # a callable object that takes a single parameter, which is the 
+    # time that the watchdog was last fed
+    # -> Note that this handler may be called multiple times, and
+    #    it will be called from the watchdog thread.
+    watchdog_error_handler = None
 
+    def __init__(self):
+    
+        self.enabled = False
+        self.fed = None
+        self.alive = True
+        self.expiration = SnakeWatchdog.kDefaultWatchdogExpiration
+        
+        self.lock = threading.RLock()
+        self.cv = threading.Condition(self.lock)
+        
+        self.thread = threading.Thread(target=self._watcher_fn)
+        self.thread.daemon = True
+        self.thread.start()
+        
+    def Feed(self):
+        with self.lock:
+            if not self.enabled:
+                return False
+                
+            was_alive = self.alive
+            self.alive = True
+            self.fed = time.time()
+            self.cv.notify()
+        
+        return was_alive
+            
+    def GetEnabled(self):
+        with self.lock:
+            return self.enabled
+    
+    def GetExpiration(self):
+        with self.lock:
+            raise self.expiration
+        
+    def GetTimer(self):
+        with self.lock:
+            return time.time() - self.fed
+            
+    def IsAlive(self):
+        with self.lock:
+            return self.alive
+        
+    def IsSystemActive(self):
+        return self.IsAlive()
+        
+    def Kill(self):
+        raise NotImplementedError()
+    
+    def SetEnabled(self, enabled):
+        with self.lock:
+            self.enabled = bool(enabled)
+            self.alive = True
+            self.fed = time.time()
+            self.cv.notify()
+    
+    def SetExpiration(self, expiration):
+        with self.lock:
+            self.expiration = float(expiration)
+            if self.enabled:
+                self.alive = True
+                self.fed = time.time()
+            self.cv.notify()
+    
+    def _watcher_fn(self):
+        '''
+            This function runs in a separate thread, and checks the
+            watchdog and determines whether it has starved or not
+        '''
+        
+        with self.lock:
+            while True:
+            
+                # use the condition variable to wait for the dog to be enabled
+                # -> note that condition variables release the lock while 
+                #    they are waiting to be notified of a change
+                self.cv.wait_for(lambda: self.enabled)
+                
+                # once enabled, ensure that the dog is being fed on time
+                while True:
+                    
+                    self.cv.wait(timeout=self.expiration)
+                    
+                    # did someone disable us?
+                    if not self.enabled:
+                        break
+                    
+                    # check for hunger
+                    now = time.time()
+                    period = now - self.fed
+                    
+                    if period > self.expiration:
+                        # looks like someone forgot to feed the watchdog!
+                        self.alive = False
+                        
+                        if SnakeWatchdog.watchdog_error_handler is not None:
+                            SnakeWatchdog.watchdog_error_handler(self.fed)
+                        else:                    
+                            print('WATCHDOG FAILURE! Last fed %0.3f seconds ago (expiration: %0.3f seconds)' % 
+                                  (period, self.expiration), file=sys.stderr)
+
+fake_wpilib.Watchdog = SnakeWatchdog
 
 
