c2416aa28c7982db4bfe5c65845ec1d7dbd979ea
Dustin Spicuzza <dustin@virtualroadside.com>
2013-03-20 16:58:28 -0400

    Add launcher for 2013 code

---
+++ launchers/2013.py
@@ -0,0 +1,24 @@
+'''
+    Generic fake_wpilib launching script for RobotSnake
+    
+    -> This file is copied to all robots, and should not need to be
+    modified at all
+'''
+
+
+import sys
+import os.path
+
+# setup the path correctly for the snake package to be loaded
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+import snake
+
+# directory that robot.py is located, relative to this file
+# -> this automatically sets the path to a directory above, with the same
+#    directory name that this file is in
+robot_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', '2013', 'robot', 'source')) 
+
+
+def run_tests(robot_module, myrobot):
+    snake.launch_robot(robot_module, myrobot)
